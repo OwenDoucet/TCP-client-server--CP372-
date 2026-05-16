cbe6a40057547ae1e1cbc64c292b32ceebd9e2a2
Winkev <37921973+Winkev@users.noreply.github.com>
2026-05-16 14:15:05 -0400

    Added a way for server to terminate
    Added if statement to check user

--- server.py
+++ server.py
@@ -1,18 +1,22 @@
 import socket
+from threading import Thread
+
 
 #config stuff
 HOST = '127.0.0.1'
 PORT = 65432
 
+running = True
+        
 def start_server():
+    global running
     #creating tcp socket
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen() #listening for connections
         print(f"Server has been started on {HOST}, {PORT}. Waiting for client connection...")
 
-        while True:
-            #accepting a connection
+        while running:
             conn, addr = s.accept()
             with conn:
                 print(f"client connected from {addr}")
@@ -26,7 +30,17 @@ def start_server():
                     message = data.decode('utf-8')
                     print(f"Message received: {message}")
 
+                    # check if message is "quit from client"
+                    if message.upper() == "QUIT":
+                        break
+
                     conn.sendall(f"ACK: {message}".encode('utf-8'))
                 print("Client disconnected")
+
+                #FOR NOW, SERVER TERMINATES WHEN CLIENT CONNECTECTION ENDS
+                running = False
+                
+                
 if __name__ == "__main__":
     start_server()
+ 
