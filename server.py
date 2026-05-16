e698708e9b54a5c6bd11fccd294b24ded59af087
Winkev <37921973+Winkev@users.noreply.github.com>
2026-05-16 14:53:37 -0400

    Added threading support
    This is so server can still run after client disconnects and server and exit manually

--- server.py
+++ server.py
@@ -1,11 +1,30 @@
 import socket
 import os
+from threading import Thread
+import time
 
 #config stuff
 HOST = '127.0.0.1'
 PORT = 65432
 
+server_run = True
+
+
+def get_input():
+    """
+    Get user input from server side
+    QUIT (or quit) will terminate server
+    """
+    global server_run
+    while server_run:
+        time.sleep(0.5)
+        server_cmd = input("Server: ")
+        if server_cmd.upper() == "QUIT":
+            server_run = False
+            print("Server Terminated")
+
 def start_server():
+    global server_run 
     # Create directory to store uploaded files
     if not os.path.exists('server_files'):
         os.makedirs('server_files')
@@ -14,11 +33,18 @@ def start_server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen() #listening for connections
+
+        s.settimeout(1.0)
         print(f"Server has been started on {HOST}, {PORT}. Waiting for client connection...")
 
-        while True:
+
+
+        while server_run:
             #accepting a connection
-            conn, addr = s.accept()
+            try:
+                conn, addr = s.accept()
+            except socket.timeout:
+                continue
             with conn:
                 print(f"client connected from {addr}")
                 
@@ -64,10 +90,17 @@ def start_server():
                         print(f"Message received: {message}")
                         if message.upper() == "QUIT":
                             print("Client requested to quit. Shutting down server.")
-                            return
+                            break
                         conn.sendall(message.encode('utf-8'))
 
                         
                 print("Client disconnected")
 if __name__ == "__main__":
-    start_server()
+    t1 = Thread(target=start_server)
+    t2 = Thread(target=get_input)
+    t2.daemon = True
+
+    t1.start()
+    t2.start()
+
+    t1.join()
